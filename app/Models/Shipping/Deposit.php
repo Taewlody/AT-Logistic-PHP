590b5d5312606eb651c601b113b83610923c2f98
taewlody <taewlody@gmail.com>
2024-01-13 18:05:55 +0700

    add shipping model

--- app/Models/Shipping/Deposit.php
+++ app/Models/Shipping/Deposit.php
@@ -8,4 +8,54 @@ use Illuminate\Database\Eloquent\Model;
 class Deposit extends Model
 {
     use HasFactory;
+
+    protected $table = 'deposit';
+
+    protected $fillable = [
+        'comCode',
+        'documentID',
+        'documentDate',
+        'refJobNo',
+        'agentCode',
+        'cusCode',
+        'payType',
+        'payTypeOther',
+        'branch',
+        'chequeNo',
+        'dueDate',
+        'note',
+        'remark',
+        'documentstatus',
+        'createID',
+        'createTime',
+        'editID',
+        'editTime',
+        'sumTotal',
+        'accountCode',
+        'dueTime',
+    ];
+
+    protected $casts = [
+        'comCode' => 'string',
+        'documentID' => 'string',
+        'documentDate' => 'date: Y-m-d',
+        'refJobNo' => 'string',
+        'agentCode' => 'string',
+        'cusCode' => 'string',
+        'payType' => 'string',
+        'payTypeOther' => 'string',
+        'branch' => 'string',
+        'chequeNo' => 'string',
+        'dueDate' => 'date: Y-m-d',
+        'note' => 'string',
+        'remark' => 'string',
+        'documentstatus' => 'string',
+        'createID' => 'string',
+        'createTime' => 'datetime:Y-m-d H:M',
+        'editID' => 'string',
+        'editTime' => 'datetime:Y-m-d H:M',
+        'sumTotal' => 'float',
+        'accountCode' => 'string',
+        'dueTime' => 'time: H:M',
+    ];
 }
