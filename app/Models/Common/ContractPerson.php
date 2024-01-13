d423765ba4a7dd25ee7cda91feca82c74bd8004d
taewlody <taewlody@gmail.com>
2024-01-13 22:09:42 +0700

    add common, customer model

---
+++ app/Models/Common/ContractPerson.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContractPerson extends Model
+{
+    use HasFactory;
+
+    protected $table = 'common_contractperson';
+
+    protected $fillable = [
+        'comCode',
+        'contactCode',
+        'cusCode',
+        'contactNameEN',
+        'contactNameTH',
+        'taxID',
+        'th_addressNo',
+        'th_moo',
+        'th_village',
+        'th_soi',
+        'th_road',
+        'th_subdistrict',
+        'th_district',
+        'th_province',
+        'zipCode',
+        'th_addressbill',
+        'en_addressNo',
+        'en_moo',
+        'en_addressbill',
+        'en_village',
+        'en_soi',
+        'en_road',
+        'en_subdistrict',
+        'en_district',
+        'en_province',
+        'countryCode',
+        'tel',
+        'fax',
+        'mobile',
+        'mail',
+        'lineID',
+        'website',
+        'note',
+        'IsActive',
+        'createID',
+        'createTime',
+        'editID',
+        'editTime',
+    ];
+
+    protected $casts = [
+        'comCode' => 'string',
+        'contactCode' => 'string',
+        'cusCode' => 'string',
+        'contactNameEN' => 'string',
+        'contactNameTH' => 'string',
+        'taxID' => 'string',
+        'th_addressNo' => 'string',
+        'th_moo' => 'string',
+        'th_village' => 'string',
+        'th_soi' => 'string',
+        'th_road' => 'string',
+        'th_subdistrict' => 'string',
+        'th_district' => 'string',
+        'th_province' => 'string',
+        'zipCode' => 'string',
+        'th_addressbill' => 'string',
+        'en_addressNo' => 'string',
+        'en_moo' => 'string',
+        'en_addressbill' => 'string',
+        'en_village' => 'string',
+        'en_soi' => 'string',
+        'en_road' => 'string',
+        'en_subdistrict' => 'string',
+        'en_district' => 'string',
+        'en_province' => 'string',
+        'countryCode' => 'string',
+        'tel' => 'string',
+        'fax' => 'string',
+        'mobile' => 'string',
+        'mail' => 'string',
+        'lineID' => 'string',
+        'website' => 'string',
+        'note' => 'string',
+        'IsActive' => BooleanString::class,
+        'createID' => 'string',
+        'createTime' => 'datetime:Y-m-d H:M',
+        'editID' => 'string',
+        'editTime' => 'datetime:Y-m-d H:M',
+    ];
+}
