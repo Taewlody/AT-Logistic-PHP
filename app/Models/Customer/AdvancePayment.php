45ef807f494685e5ea2daedfd48c6286cfc5983b
taewlody <taewlody@gmail.com>
2024-01-13 17:50:36 +0700

    add customer model

---
+++ app/Models/Customer/AdvancePayment.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdvancePayment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'advance_payment';
+
+    protected $fillable = [
+        'comCode',
+        'documentID',
+        'documentDate',
+        'refJobNo',
+        'cusCode',
+        'payType',
+        'payTypeOther',
+        'branch',
+        'chequeNo',
+        'dueDate',
+        'note',
+        'remark',
+        'documentstatus',
+        'createID',
+        'createTime',
+        'editID',
+        'editTime',
+        'sumTotal',
+        'accountCode',
+        'dueTime'
+    ];
+
+    protected $casts = [
+        'comCode' => 'string',
+        'documentID' => 'string',
+        'documentDate' => 'date: Y-m-d',
+        'refJobNo' => 'string',
+        'cusCode' => 'string',
+        'payType' => 'string',
+        'payTypeOther' => 'string',
+        'branch' => 'string',
+        'chequeNo' => 'string',
+        'dueDate' => 'date: Y-m-d',
+        'note' => 'string',
+        'remark' => 'string',
+        'documentstatus' => 'string',
+        'createID' => 'string',
+        'createTime' => 'datetime:Y-m-d H:M',
+        'editID' => 'string',
+        'editTime' => 'datetime:Y-m-d H:M',
+        'sumTotal' => 'float',
+        'accountCode' => 'string',
+        'dueTime' => 'string'
+    ];
+}
