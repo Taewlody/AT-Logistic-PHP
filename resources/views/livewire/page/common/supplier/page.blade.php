877f50dae812baa47f53862dec8f7754342d646e
taewlody <taewlody@gmail.com>
2024-02-03 16:08:43 +0700

    common data supplier page

--- resources/views/livewire/page/common/supplier/page.blade.php
+++ resources/views/livewire/page/common/supplier/page.blade.php
@@ -1,59 +1,67 @@
 <div>
     <livewire:component.page-heading title_main="Supplier" title_sub="ผู้จำหน่าย" breadcrumb_title="Common Data" breadcrumb_page="Supplier" />
 
-    <div class="wrapper wrapper-content animated fadeInRight">
+    <div class="container-fluid">
 
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ibox ">
-                    <div class="ibox-title">
-                        <h5>Country List</h5>
-                        <div class="ibox-tools"> <a href="country_form?action=add" class="btn btn-primary btn-xs"><i
-                                    class="fa fa-plus "> </i> Create new </a> </div>
-                    </div>
-                    <div class="ibox-content">
-                        <div class="table-responsive">
-                            <input type="text" class="form-control form-control-sm m-b-xs"
-                                id="filter"placeholder="Search in table">
+        <div class="card ">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col-7"></div>
+                        <div class="col-3">
+                            <input type="text" id="search" name="search" class="form-control" wire:model.live="searchText" placeholder="Search in table">
+                        </div>
+                        <div class="col-2" style="justify-content: flex-end; display: flex">
+                            <a href="country_form?action=add" class="btn btn-primary">
+                                <i class="fa fa-plus "> </i> Create new 
+                            </a>
+                        </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
 
-                            {{-- <table></table> --}}
-                            {{-- <livewire:page.common.country.table :data="$countries"/> --}}
+                    <table class="footable table table-stripped toggle-arrow-tiny"
+                        data-page-size="{{ $data->total() }}" data-filter=#filter>
+                        <thead>
+                            <tr>
+                                <th style="width:5%">No.</th>
+                                <th style="width:10%">Code</th>
+                                <th style="width:40%">Supplier Name</th>
+                                <th  style="width:10%">Status</th>
+                                <th data-hide="phone"  style="width:10%">Update By</th>
+                                <th data-hide="phone,tablet"  style="width:10%">Action</th>
+                            </tr>
+                          </thead>
+                            <tbody>
+                                @foreach ($data as $item)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->supCode }}</td>
+                                    <td>{{ $item->supNameTH }}</td>
+                                    <td class="center"><span @class(['badge', 'label-primary' => $item->isActive])>{{ $item->isActive ? 'Active' : 'Disable' }}</span></td>
+                                    <td class="center">{{ $item->editBy != null? $item->editBy->username : '' }}</td>
+                                    <td>
+                                        <div class="btn-group">
+                                            <button class="btn btn-xs btn-success" onClick="location.href='port_form?action=view&portCode={{ $item->supCode }}">View</button>
+                                            <button class="btn btn-xs btn-primary" onClick="location.href='port_form?action=edit&portCode={{ $item->supCode }}">Edit</button>
+                                            <button class="btn btn-xs btn-danger" onClick="return confirmDel('{{ $item->supCode }}','port_action.php');">Delete</button>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                    </table>
+                    <br/>
 
-                            <table class="footable table table-stripped toggle-arrow-tiny"
-                                data-page-size="{{ $data->total() }}" data-filter=#filter>
-                                <thead>
-                                    <tr>
-                                        <th style="width:5%">No.</th>
-                                        <th style="width:10%">Code</th>
-                                        <th style="width:40%">Supplier Name</th>
-                                        <th  style="width:10%">Status</th>
-                                        <th data-hide="phone"  style="width:10%">Update By</th>
-                                        <th data-hide="phone,tablet"  style="width:10%">Action</th>
-                                    </tr>
-                                  </thead>
-                                    <tbody>
-                                        @foreach ($data as $item)
-                                        <tr>
-                                            <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $item->supCode }}</td>
-                                            <td>{{ $item->supNameTH }}</td>
-                                            <td class="center"><span @class(['label', 'label-primary' => $item->isActive])>{{ $item->isActive ? 'Active' : 'Disable' }}</span></td>
-                                            <td class="center">{{ $item->editBy != null? $item->editBy->username : '' }}</td>
-                                            <td>
-                                                <div class="btn-group">
-                                                    <button class="btn-white btn btn-xs" onClick="location.href='port_form?action=view&portCode={{ $item->supCode }}">View</button>
-                                                    <button class="btn-white btn btn-xs" onClick="location.href='port_form?action=edit&portCode={{ $item->supCode }}">Edit</button>
-                                                    <button class="btn-white btn btn-xs" onClick="return confirmDel('{{ $item->supCode }}','port_action.php');">Delete</button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        @endforeach
-                            </table>
-                            {{ $data->appends(['sort'])->links() }}
-
-
+                    <div class="row">
+                        <div class="col-4">
+                            {{ $data->withQueryString()->links('layouts.themes.layout.custom-pagination-info') }}
+                        </div>
+                        <div class="col-8"> 
+                            {{ $data->withQueryString()->links('layouts.themes.layout.custom-pagination-links') }}
                         </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
