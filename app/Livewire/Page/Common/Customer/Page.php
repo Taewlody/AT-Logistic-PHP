defe175560b55f39c9dca7f0a661de13e08edea2
taewlody <taewlody@gmail.com>
2024-02-03 16:03:18 +0700

    common data customer page

--- app/Livewire/Page/Common/Customer/Page.php
+++ app/Livewire/Page/Common/Customer/Page.php
@@ -9,8 +9,15 @@ use App\Models\Common\Customer;
 class Page extends Component
 {
     use WithPagination;
+    public $searchText = "";
+    
     public function render()
     {
-        return view('livewire.page.common.customer.page', [ 'data'=> Customer::paginate(50)])->extends('layouts.main')->section('main-content');
+        return view('livewire.page.common.customer.page', [ 
+            'data'=> Customer::where('cusCode', 'like', '%'.$this->searchText.'%')
+            ->orWhere('custNameEN', 'like', '%'.$this->searchText.'%')
+            ->orWhere('custNameTH', 'like', '%'.$this->searchText.'%')
+            ->paginate(20)
+            ])->extends('layouts.main')->section('main-content');
     }
 }
