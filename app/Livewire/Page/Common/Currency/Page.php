83b151dee52b35adc1cfeddfba859a30c52d2262
taewlody <taewlody@gmail.com>
2024-02-03 20:02:04 +0700

    common data currency page

--- app/Livewire/Page/Common/Currency/Page.php
+++ app/Livewire/Page/Common/Currency/Page.php
@@ -9,8 +9,15 @@ use App\Models\Common\Currency;
 class Page extends Component
 {
     use WithPagination;
+    public $searchText = "";
+    
     public function render()
     {
-        return view('livewire.page.common.currency.page', [ 'data'=> Currency::paginate(50)])->extends('layouts.main')->section('main-content');
+        return view('livewire.page.common.currency.page', [ 
+            'data'=> Currency::where('currencyCode', 'like', '%'.$this->searchText.'%')
+            ->orWhere('currencyName', 'like', '%'.$this->searchText.'%')
+            ->orWhere('exchange_rate', 'like', '%'.$this->searchText.'%')
+            ->paginate(20)
+            ])->extends('layouts.main')->section('main-content');
     }
 }
