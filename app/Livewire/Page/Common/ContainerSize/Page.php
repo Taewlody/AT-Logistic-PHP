0a265caf3512c8392dd061452aef6622952c7fd3
taewlody <taewlody@gmail.com>
2024-02-03 19:52:24 +0700

    common data container size page

--- app/Livewire/Page/Common/ContainerSize/Page.php
+++ app/Livewire/Page/Common/ContainerSize/Page.php
@@ -9,8 +9,14 @@ use App\Models\Common\ContainerSize;
 class Page extends Component
 {
     use WithPagination;
+    public $searchText = "";
+
     public function render()
     {
-        return view('livewire.page.common.container-size.page', [ 'data'=> ContainerSize::paginate(50)])->extends('layouts.main')->section('main-content');
+        return view('livewire.page.common.container-size.page', [ 
+            'data'=> ContainerSize::where('containersizeCode', 'like', '%'.$this->searchText.'%')
+            ->orWhere('containersizeName', 'like', '%'.$this->searchText.'%')
+            ->paginate(20)
+            ])->extends('layouts.main')->section('main-content');
     }
 }
