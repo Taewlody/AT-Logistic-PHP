05a774b7ab0e59f35acc9c2537999b6d3edbb21b
wrb <68177302+notheartza@users.noreply.github.com>
2024-02-06 10:29:55 +0700

    add page

---
+++ app/Livewire/Page/Common/ContainerSize/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Page\Common\ContainerSize;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.common.container-size.form');
+    }
+}
---
+++ app/Livewire/Page/Common/Unit/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Page\Common\Unit;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.common.unit.form');
+    }
+}
