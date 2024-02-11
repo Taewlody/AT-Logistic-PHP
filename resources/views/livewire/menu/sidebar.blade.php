22f6d6304ac9bc10436b13376f70394b9ce92c25
taewlody <taewlody@gmail.com>
2024-02-11 16:38:55 +0700

    edit scrollbar sidebar

--- resources/views/livewire/menu/sidebar.blade.php
+++ resources/views/livewire/menu/sidebar.blade.php
@@ -66,7 +66,7 @@
             <div id="sidebar-menu">
 
                 <ul class="sidebar-links" id="simple-bar">
-                    {{-- <li class="back-btn">
+                    <li class="back-btn">
                         <a href="{{ route('dashboard') }}"><img class="img-fluid for-light"
                                 src="{{ asset('assets/images/logo/logo-icon.png') }}" alt="">
                             <img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo-icon-dark.png') }}"
@@ -77,51 +77,6 @@
                             <i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
                         </div>
                     </li>
-
-                    <li class="sidebar-list">
-                        <a class="sidebar-link sidebar-title" href="javascript:void(0)">
-                            <i data-feather="home"></i>
-                            <span class="lan-3">Dashboard</span>
-                        </a>
-                        <ul class="sidebar-submenu">
-                            <li><a class="lan-4" href="{{ route('dashboard') }}">Dashboard</a></li>
-                        </ul>
-                    </li> --}}
-
-                    {{-- @foreach ($mainMenu as $menu)
-                    <li :class="{ 'active': activeMenu === '{{ $menu['menu_name'] }}' }">
-                        <a href="#" x-on:click="update('{{ $menu['menu_name'] }}')" aria-expanded="false">
-                            <i class="{{ $menu['icon'] }}"></i>
-                            <span class="nav-label">{{ $menu['name'] }}</span>
-                            <span class="fa arrow"></span>
-                        </a>
-                        @if (array_key_exists('menu', $menu))
-                            <ul class="nav nav-second-level collapse"
-                                :class="{ 'in': activeMenu === '{{ $menu['menu_name'] }}' }">
-                                @foreach ($menu['menu'] as $sub_menu)
-                                    <li>
-                                        <a
-                                            href="@if (Route::has($sub_menu['route_name'])) {{ route($sub_menu['route_name']) }} @else # @endif">{{ $sub_menu['name'] }}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </li>
-
-                    
-                @endforeach --}}
-
-                    {{-- <li class="back-btn">
-                        <a href="{{ route('dashboard') }}"><img class="img-fluid for-light"
-                                src="{{ asset('assets/images/logo/logo-icon.png') }}" alt="">
-                            <img class="img-fluid for-dark" src="{{ asset('assets/images/logo/logo-icon-dark.png') }}"
-                                alt="">
-                        </a>
-                        <div class="mobile-back text-end">
-                            <span>Back</span>
-                            <i class="fa fa-angle-right ps-2" aria-hidden="true"></i>
-                        </div>
-                    </li> --}}
 
                     @foreach ($mainMenu as $menu)
                         <li class="sidebar-list">
