6c76a8ca20d0ae6390cd28b313345270de7f52c1
taewlody <taewlody@gmail.com>
2024-03-03 18:17:47 +0700

    edit customdatetime

--- app/Casts/CustomDateTime.php
+++ app/Casts/CustomDateTime.php
@@ -10,10 +10,19 @@ use Illuminate\Support\Facades\Log;
 class CustomDateTime implements CastsAttributes {
     public function get(Model $model, string $key, mixed $value, array $attributes) {
         // Log::debug("get value date: $value");
-        return $value == "0000-00-00 00:00:00" || $value == "" ? Carbon::createFromTimestamp(0)->setTimezone('UTC')->format('Y-m-d H:i:s') : Carbon::parse($value)->setTimezone('UTC')->format('Y-m-d H:i:s');
+        
+        if($value == "") {
+            return Carbon::createFromTimestamp(0)->setTimezone('UTC')->format('Y-m-d H:i:s');
+        } else if($value < 1) {
+            return '';
+        } else {
+            return Carbon::parse($value)->setTimezone('UTC')->format('Y-m-d H:i:s');
+        }
     }
     public function set(Model $model, string $key, mixed $value, array $attributes) {
         // Log::debug("set value date [".gettype($value)."]: $value");
-        return $value == "0000-00-00 00:00:00" || $value == "" ? Carbon::createFromTimestamp(0)->setTimezone('UTC')->format('Y-m-d H:i:s') : Carbon::parse($value)->setTimezone('UTC')->format('Y-m-d H:i:s');
+        
+        return $value == "" ? '0000-00-00 00:00:00' : Carbon::parse($value)->setTimezone('UTC')->format('Y-m-d H:i:s');
+
     }
 }
