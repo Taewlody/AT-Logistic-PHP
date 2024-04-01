52e9d664be15a44d588ce191219d684189e0aecc
taewlody <taewlody@gmail.com>
2024-04-01 17:39:53 +0700

    report sale tax invoice

--- app/Livewire/Page/Report/ReportSaleInvoice/Page.php
+++ app/Livewire/Page/Report/ReportSaleInvoice/Page.php
@@ -6,15 +6,59 @@ use Livewire\Component;
 use Livewire\WithPagination;
 
 use App\Models\Account\Invoice;
+use App\Models\Common\Customer;
+use App\Models\Common\Saleman;
 
 class Page extends Component
 {
     use WithPagination;
+
+    public $query = [];
+    public $dateStart;
+    public $dateEnd;
+    public $documentID = '';
+    public $customerList = [];
+    public $customerSearch = "";
+    public $salemanList = [];
+    public $salemanSearch = "";
+
+    #[On('post-search')] 
+    public function search() {
+        $this->query = [];
+
+        if($this->dateStart != null) {
+            $this->query[] = ['documentDate', '>=', $this->dateStart];
+        }
+        if($this->dateEnd != null) {
+            $this->query[] = ['documentDate', '<=', $this->dateEnd];
+        }
+        if($this->documentID != null) {
+            $this->query[] = ['documentID', 'like', '%'.$this->documentID.'%'];
+        }
+        if($this->customerSearch != null) {
+            $this->query[] = ['cusCode', '=', $this->customerSearch];
+        }
+        if($this->salemanSearch != null) {
+            $this->query[] = ['saleman', '=', $this->salemanSearch];
+        }
+    }
+
+    public function mount()
+    {
+        $this->dateStart = null;
+        $this->dateEnd = null;
+        $this->customerList = Customer::all()->sortBy('custNameEN');
+        $this->salemanList = Saleman::all()->sortBy('empName');
+
+    }
     
     public function render()
     {
         return view('livewire.page.report.report-sale-invoice.page', [
-            'data' => Invoice::with(['customer', 'salemanRef'])->where('documentstatus', 'A')->orderBy('documentID', 'DESC')->paginate(20) 
+            'data' => Invoice::with(['customer', 'salemanRef'])
+            ->where('documentstatus', 'A')->orderBy('documentID', 'DESC')
+            ->where($this->query)
+            ->paginate(20) 
         ])->extends('layouts.main')->section('main-content');
     }
 }
