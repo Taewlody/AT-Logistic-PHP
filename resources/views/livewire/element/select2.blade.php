d958aafb35d04a18843412e53d7736439a5711a1
taewlody <taewlody@gmail.com>
2024-04-25 16:51:40 +0700

    fix trigger change select2

--- resources/views/livewire/element/select2.blade.php
+++ resources/views/livewire/element/select2.blade.php
@@ -18,16 +18,28 @@
         minimumResultsForSearch: $wire.searchable ? 0 : Infinity,
     });
 
-    $('#{{$this->__id}}').on('change', function (e) {
-        // console.log('change item: {{$this->__id}} ->', e.target.value)
-        if($(this).data('select2')) {
-            let data = $(this).select2('val');
-            @this.set($wire.nameKey, data);
+    // $('#{{$this->__id}}').on('change', function (e) {
+    //     // console.log('change item: {{$this->__id}} ->', e.target.value)
+    //     setTimeout(() => {
+    //         console.log($(this).data('select2'), e);
+            
+    //     }, 1000);
+    //     if($(this).data('select2')) {
+    //         let data = $(this).select2('val');
+    //         @this.set($wire.nameKey, data);
+    //     }else {
+    //         console.log('select2 not found');
+    //     }
+        
+    //     // @this.set('value', e.target.value);
+    // });
+    $('#{{$this->__id}}').on('select2:select', function (e) {
+        var data = e.params.data;
+        if(data) {
+            @this.set($wire.nameKey, data.id);
         }else {
             console.log('select2 not found');
         }
-        
-        // @this.set('value', e.target.value);
     });
 </script>
 @endscript
