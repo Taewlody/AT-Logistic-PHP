c2a07193f6fd089228c64163eea5a23ab45f830d
taewlody <taewlody@gmail.com>
2024-05-10 19:10:28 +0700

    change select2

--- resources/views/livewire/page/report/report-invoice-overdue/page.blade.php
+++ resources/views/livewire/page/report/report-invoice-overdue/page.blade.php
@@ -31,14 +31,18 @@
                                     <div class="form-group col-margin0">
                                         <label class="font-normal">Cutomer</label>
                                         <div>
-                                            <select class="select2_single form-control select2"
+                                            {{-- <select class="select2_single form-control select2"
                                                 name="cusCode" id="cusCode" wire:model="customerSearch">
                                                 <option value="">- select -</option>
                                                 @foreach ($customerList as $customer)
                                                     <option value="{{ $customer->cusCode }}">
                                                         {{ $customer->custNameEN ? $customer->custNameEN : $customer->custNameTH }}</option>
                                                 @endforeach
-                                            </select>
+                                            </select> --}}
+                                            <livewire:element.select2 wire:model='customerSearch'
+                                                name="cusCode" :options="Service::CustomerSelecter()"
+                                                itemKey="cusCode" itemValue="custNameEN" 
+                                                :searchable="true">
                                         </div>
                                     </div>
                                 </div>
@@ -46,14 +50,18 @@
                                     <div class="form-group col-margin0">
                                         <label class="font-normal">Sale</label>
                                         <div>
-                                            <select class="select2_single form-control select2"
+                                            {{-- <select class="select2_single form-control select2"
                                             name="saleman" id="saleman" wire:model="salemanSearch">
-                                            <option value="">- select -</option>
-                                            @foreach ($salemanList as $saleman)
-                                                <option value="{{ $saleman->usercode }}">
-                                                    {{ $saleman->empName }}</option>
-                                            @endforeach
-                                    </select>
+                                                <option value="">- select -</option>
+                                                @foreach ($salemanList as $saleman)
+                                                    <option value="{{ $saleman->usercode }}">
+                                                        {{ $saleman->empName }}</option>
+                                                @endforeach
+                                            </select> --}}
+                                            <livewire:element.select2 wire:model='salemanSearch'
+                                                name="salemanSearch" :options="Service::AllSalemanSelecter()"
+                                                itemKey="usercode" itemValue="empName" 
+                                                :searchable="true">
                                         </div>
                                     </div>
                                 </div>
